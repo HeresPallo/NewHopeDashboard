57dfa203d635df2be55e4e4f15e05675ff6cfba8
Pallo Bangura <herespallo@gmail.com>
2025-02-12 18:59:14 +0000

    changed the react dependencies and deleted table from the overview admin folder

--- src/vite.config.js
+++ src/vite.config.js
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react';
 // Vite configuration
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',  // Ensure Vite builds to the correct directory
-  },
 });
