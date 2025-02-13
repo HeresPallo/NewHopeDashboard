8fe2b4e27cc41c2d822f4d2b4fbc2265d554823d
Pallo Bangura <herespallo@gmail.com>
2025-02-13 17:19:38 +0000

    updated thumbnail handling

--- src/components/News/ViewNews.jsx
+++ src/components/News/ViewNews.jsx
@@ -76,13 +76,14 @@ const ViewNews = () => {
       </div>
 
       {/* Thumbnail */}
-{news.thumbnail && (
+      {news.thumbnail && (
   <img
-    src={news.thumbnail} // This should now be a full S3 URL
+    src={news.thumbnail} // Ensure this is the complete S3 URL
     alt="News Thumbnail"
     className="w-full h-64 object-cover rounded-md mb-6"
   />
 )}
+
       {/* News Content */}
       <p className="text-gray-700 leading-relaxed">{news.content}</p>
 
