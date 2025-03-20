5c202659251c99dfe682ace295ecb525be30a44b
Pallo Bangura <herespallo@gmail.com>
2025-03-20 23:10:27 +0000

    updated jwtdecode

--- src/components/News/CreateNews.jsx
+++ src/components/News/CreateNews.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import jwtDecode from "jwt-decode";
+import { default as jwtDecode } from "jwt-decode";
+
 
 const CreateNews = () => {
   const navigate = useNavigate();
